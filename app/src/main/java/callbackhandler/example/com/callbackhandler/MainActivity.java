82759254d21111d175a69a646a7856796f3e5ffa
PoisonStr <1104183457@qq.com>
2018-05-06 10:35:13 +0800

    33333

---
+++ app/src/main/java/callbackhandler/example/com/callbackhandler/MainActivity.java
@@ -0,0 +1,11 @@
+package callbackhandler.example.com.callbackhandler;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class MainActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/callbackhandler/example/com/callbackhandler/MyButton.java
@@ -0,0 +1,24 @@
+package callbackhandler.example.com.callbackhandler;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Button;
+
+import java.util.jar.Attributes;
+
+public class MyButton extends android.support.v7.widget.AppCompatButton {
+public  MyButton(Context context, AttributeSet set){
+    super(context,set);
+}
+    @Override
+    public  boolean onKeyDown(int KeyCode, KeyEvent event){
+    super.onKeyDown(KeyCode,event);
+        Log.v("-crazyit.org-","the onKeyDown in MyButton");
+        return true;
+
+    }
+}
